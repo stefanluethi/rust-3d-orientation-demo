c40c3615d6a59e5a2974fdf8cb1a7c3f124c8661
Stefan Lüthi <stefan.luethi@zapp.ch>
2020-11-05 13:42:49 +0100

    Add serial data print

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,16 @@ use embedded_hal::spi::Mode;
 
 extern crate lsm6dso;
 use lsm6dso::{Lsm6dso, SlaveAddr, RawAccelerometer, Error, Accelerometer};
+use stm32f4xx_hal::rcc::Clocks;
 
+use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
     //rtt_target::rtt_init_print!();
 
-    let mut dp = stm32::Peripherals::take().unwrap();
-    let mut cp = cortex_m::peripheral::Peripherals::take().unwrap();
+    let mut dp = stm32::Peripherals::take().expect("cannot take stm32 peripherals");
+    let mut cp = cortex_m::peripheral::Peripherals::take().expect("cannot take cortex peripherals");
 
     /* delay */
     // Set up the system clock. We want to run at 48MHz for this one.
@@ -43,18 +45,30 @@ fn main() -> ! {
     // Create a delay abstraction based on SysTick
     let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
 
+    /* gpio's */
+    let gpioa = dp.GPIOA.split();
+    let gpiob = dp.GPIOB.split();
+    let gpioc = dp.GPIOC.split();
+
+    /* uart */
+    let txd = gpioa.pa2.into_alternate_af7();
+    let rxd = gpioa.pa3.into_alternate_af7();
+    let mut serial = hal::serial::Serial::usart2(
+        dp.USART2,
+        (txd, rxd),
+        hal::serial::config::Config::default().baudrate(115_200.bps()),
+        clocks
+    ).unwrap();
+    let (mut tx, _rx) = serial.split();
+
     /* itm output */
     let stim = &mut cp.ITM.stim[1];
 
     /* GPIOs */
-    let gpioa = dp.GPIOA.split();
     let mut led = gpioa.pa5.into_push_pull_output();
-
-    let gpioc = dp.GPIOC.split();
     let button = gpioc.pc13.into_floating_input();
 
     /* sensors */
-    let gpiob = dp.GPIOB.split();
     let sda = gpiob.pb9.into_alternate_af4_open_drain();
     let scl = gpiob.pb8.into_alternate_af4_open_drain();
     let i2c = hal::i2c::I2c::i2c1(
@@ -70,29 +84,28 @@ fn main() -> ! {
         SlaveAddr::Alternate
     ).expect("LSM3DSO could not be initalized");
     cortex_m::iprintln!(stim, "init. sensor finished");
-    acc.set_range(lsm6dso::Range::G8);
+    acc.set_range(lsm6dso::Range::G2);
 
     let mut my_object = Stm32Io::new(led, button);
 
     loop {
         my_object.do_something();
         let acceleration = acc.accel_norm().unwrap();
-        cortex_m::iprintln!(
-            stim,
+        writeln!(tx,
             "{{\
                 \"meas\":\"acc\",\
-                \"values\":[\
+                \"values\":{{\
                     \"x\":{},\
                     \"y\":{},\
                     \"z\":{}\
-                ],\
+                }},\
                 \"unit\":\"g\"\
             }}",
             acceleration.x,
             acceleration.y,
             acceleration.z
-        );
-        delay.delay_ms(500_u16);
+        ).unwrap();
+        delay.delay_ms(50_u16);
     }
 
     loop {}
